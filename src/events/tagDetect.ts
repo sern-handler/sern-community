271731f53a9591f946ea47c86b1c0a3503b2ecbe
EvolutionX <jeyush2005@gmail.com>
2022-08-15 12:36:08 +0530

    chore: reduce sensitivity for trigger

--- src/events/tagDetect.ts
+++ src/events/tagDetect.ts
@@ -21,7 +21,7 @@ export default eventModule({
 			keywords
 		);
 
-		if (matches.bestMatch.rating < 0.7) return;
+		if (matches.bestMatch.rating <= 0.75) return;
 		const tag = file.find((t) => t.keywords.includes(matches.bestMatch.target));
 		if (!tag) return;
 		const mention = message.mentions.users.first();
@@ -34,7 +34,7 @@ export default eventModule({
 		const embed = new EmbedBuilder()
 			.setDescription(tag.content.trim())
 			.setFooter({
-				text: `Triggered by ${message.author.tag}`,
+				text: `Triggered by ${message.author.tag} | Confidence: ${matches.bestMatch.rating}`,
 				iconURL: message.author.displayAvatarURL(),
 			})
 			.setColor("Random")
