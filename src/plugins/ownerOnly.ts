8ca5e0c3ea075c0370b11613574c2f7c6856e1e0
Neo <80315475+NeoYaBoi@users.noreply.github.com>
2022-08-19 08:12:57 +1000

    fix

--- src/plugins/ownerOnly.ts
+++ src/plugins/ownerOnly.ts
@@ -1,5 +1,5 @@
 import { CommandType, EventPlugin, PluginType } from "@sern/handler";
-const ownerIDs = ["697795666373640213", "182326315813306368"];
+const ownerIDs = ["697795666373640213", "182326315813306368", "756393473430519849"];
 export function ownerOnly(): EventPlugin<CommandType.Both> {
 	return {
 		type: PluginType.Event,
