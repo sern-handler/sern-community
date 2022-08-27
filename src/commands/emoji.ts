18cb0cb89297d27f5b7dcce4b6a805de05e4a320
EvolutionX <jeyush2005@gmail.com>
2022-08-27 19:32:03 +0530

    chore: publish and lint

--- src/commands/emoji.ts
+++ src/commands/emoji.ts
@@ -1,46 +1,56 @@
 import { commandModule, CommandType } from "@sern/handler";
 import { ApplicationCommandOptionType, Attachment } from "discord.js";
+import { publish } from "../plugins/publish";
 
 export default commandModule({
 	type: CommandType.Slash,
+	plugins: [publish({ dmPermission: false })],
 	options: [
 		{
-			name: 'submit',
+			name: "submit",
 			type: ApplicationCommandOptionType.Subcommand,
-			description: 'submit an emoji',
+			description: "submit an emoji",
 			options: [
 				{
-					name: 'attachment',
+					name: "attachment",
 					type: ApplicationCommandOptionType.Attachment,
-					description: 'an attachment for submission',
-					required: true
-				}
-			]
-		}
+					description: "An attachment for submission",
+					required: true,
+				},
+			],
+		},
 	],
 	async execute(ctx, [, args]) {
 		const command = args.getSubcommand();
 
 		switch (command) {
-			case 'submit' : {
-				const attachment =  args.getAttachment('attachment', true)
+			case "submit": {
+				const attachment = args.getAttachment("attachment", true);
 				const verifyAttachment = validAttachment(attachment);
 				const conditions = verifyAttachment(
-					a => (a.width ?? 0) <= 256 && (a.height ?? 0) <= 256,
-					a =>  ['png','jpg'].includes(a.contentType ?? 'Something that is not png or jpg when contentType is null')
+					(a) => (a.width ?? 0) <= 256 && (a.height ?? 0) <= 256,
+					(a) =>
+						["png", "jpg"].includes(
+							a.contentType ??
+								"Something that is not png or jpg when contentType is null"
+						)
 				);
-				if(conditions) {
-
+				if (conditions) {
 				} else {
-					await ctx.reply('Your attachment is not in valid format. Please submit something else')
+					await ctx.reply(
+						"Your attachment is not in valid format. Please submit something else"
+					);
 				}
 			}
 		}
-	}
-})
+	},
+});
 
 function validAttachment(attachment: Attachment) {
-	return (...conditions: ((attachment : Attachment) => boolean)[]) => {
-		return conditions.reduce((partial, func) => func(attachment) && partial, true)
-	}
+	return (...conditions: ((attachment: Attachment) => boolean)[]) => {
+		return conditions.reduce(
+			(partial, func) => func(attachment) && partial,
+			true
+		);
+	};
 }
