17a6c3bf5bd389a16f7fb925ee4d29ca8ba34579
Evo <85353424+EvolutionX-10@users.noreply.github.com>
2022-08-31 22:48:30 +0530

    feat: emoji system (#16)
    Co-authored-by: Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>

---
+++ src/commands/handlers/emojiModal.ts
@@ -0,0 +1,62 @@
+import { commandModule, CommandType } from "@sern/handler";
+import { ActionRowBuilder, ButtonBuilder, EmbedBuilder } from "discord.js";
+
+export default commandModule({
+	type: CommandType.Modal,
+	async execute(ctx) {
+		const emojiURL = ctx.message!.embeds[0].image?.url;
+		if (!emojiURL)
+			return ctx.reply({
+				content: "Uh something bad happened, I couldn't get the emoji!",
+				ephemeral: true,
+			});
+		const name = ctx.fields.getTextInputValue("emoji/name");
+
+		const emoji = await ctx.guild?.emojis
+			.create({
+				attachment: emojiURL,
+				name,
+				reason: `Accepted by ${ctx.user.tag}`,
+			})
+			.catch((e) => {
+				ctx.reply({
+					content: e.message,
+					ephemeral: true,
+				});
+				return null;
+			});
+
+		if (!emoji) return;
+
+		await ctx.deferUpdate();
+
+		const components = [
+			new ActionRowBuilder<ButtonBuilder>().setComponents(
+				ctx.message!.components[0].components.map((c) =>
+					new ButtonBuilder(c.data).setDisabled()
+				)
+			),
+		];
+
+		const embed = new EmbedBuilder(ctx.message?.embeds[0]?.data) //
+			.setFields(
+				{
+					name: "Emoji details",
+					value: `\` - \` Name: ${emoji?.name}\n\` - \` Animated: ${
+						emoji?.animated ? "Yes" : "Nope"
+					}\n\` - \` Emoji: ${emoji}\n\` - \` Raw: \`${emoji.toString()}\``,
+				},
+				{
+					name: "Status",
+					value: `Accepted by ${ctx.user}`,
+				}
+			)
+			.setColor("Green")
+			.setFooter({
+				text: `GG ${ctx.message?.embeds[0].author?.name}`,
+			})
+			.setTimestamp();
+
+		await ctx.message?.edit({ content: null, embeds: [embed], components, files: [] });
+	},
+});
