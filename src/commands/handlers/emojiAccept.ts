63f2d127e1bb3ffac87af3856941ae1d23e1098b
EvolutionX <jeyush2005@gmail.com>
2022-08-31 21:49:45 +0530

    feat(emojis): a lot of work

---
+++ src/commands/handlers/emojiAccept.ts
@@ -0,0 +1,41 @@
+import { commandModule, CommandType } from "@sern/handler";
+import {
+	ActionRowBuilder,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+} from "discord.js";
+import { ownerIDs } from "../../plugins/ownerOnly.js";
+
+export default commandModule({
+	type: CommandType.Button,
+	name: "emoji/accept",
+	async execute(ctx) {
+		if (!ownerIDs.includes(ctx.user.id))
+			return ctx.reply({
+				ephemeral: true,
+				content: `You really thought the devs would allow you to accept the emoji?`,
+			});
+
+		const suggestedName = ctx.message.embeds[0].fields[0].value!;
+
+		const modal = new ModalBuilder()
+			.setCustomId("emojiModal")
+			.setTitle("Emoji Creation");
+
+		const row = new ActionRowBuilder<TextInputBuilder>().setComponents(
+			new TextInputBuilder() //
+				.setCustomId("emoji/name")
+				.setLabel("Emoji Name")
+				.setMinLength(2)
+				.setMaxLength(32)
+				.setPlaceholder("Name of the emoji")
+				.setRequired()
+				.setStyle(TextInputStyle.Short)
+				.setValue(suggestedName)
+		);
+		modal.setComponents(row);
+
+		await ctx.showModal(modal);
+	},
+});
