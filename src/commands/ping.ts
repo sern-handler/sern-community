28b3252ab67c6cc90de92a4cb643124deb7245be
EvolutionX <jeyush2005@gmail.com>
2022-10-05 11:09:05 +0530

    feat: show uptime in ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,17 @@
 import { commandModule, CommandType } from "@sern/handler";
 import { publish } from "#plugins";
+import { Timestamp } from "#utils";
 
 export default commandModule({
 	type: CommandType.Slash,
 	plugins: [publish()],
 	description: "Pong!",
 	execute: async (context) => {
-		await context.reply(`Pong 🏓 \`${context.client.ws.ping}ms\``);
+		const uptime = new Timestamp(context.client.readyTimestamp!);
+		await context.reply(
+			`Pong 🏓 \`${
+				context.client.ws.ping
+			}ms\`\nStarted running ${uptime.getRelativeTime()}`
+		);
 	},
 });
