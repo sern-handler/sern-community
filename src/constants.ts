02da3e4c5a4bb954d164c9036c5140e7f14aefea
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-12-27 11:11:55 +0100

    fix: don't add mina

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const Evo = "697795666373640213";
 export const Seren = "182326315813306368";
 export const Ropox = "756393473430519849";
 export const Mina = "504698587221852172";
-export const ownerIDs = [Evo, Seren, Ropox, Mina];
+export const ownerIDs = [Evo, Seren, Ropox];
