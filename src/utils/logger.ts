62ce5903c839dc4c86ac69347cfec0953230e615
EvolutionX <evolutionx9777@gmail.com>
2023-01-04 18:21:27 +0530

    chore: pretty and uniform with other Classes

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,40 +1,40 @@
 import type { Logging, LogPayload } from "@sern/handler";
 import winston from "winston";
-import util from 'util'
+import util from "util";
 export class SernLogger implements Logging {
-	private _winston!: winston.Logger
+	private _winston!: winston.Logger;
 
-	constructor(
-		level: string,
-		isProd : boolean = false
-	) {
+	public constructor(level: string, isProd = false) {
 		this._winston = winston.createLogger({
 			level,
 			format: winston.format.json(),
 		});
-		if(!isProd) {
-			this._winston.add(new winston.transports.Console({
-				format: winston.format.simple(),
-			}))
+		if (!isProd) {
+			this._winston.add(
+				new winston.transports.Console({
+					format: winston.format.simple(),
+				})
+			);
 		} else {
-			this._winston.add(new winston.transports.File({ filename: 'combined.log' }))
+			this._winston.add(
+				new winston.transports.File({ filename: "combined.log" })
+			);
 		}
 	}
 
-	error(payload: LogPayload<unknown>): void {
-		this._winston.error(payload.message)
+	public error(payload: LogPayload<unknown>): void {
+		this._winston.error(payload.message);
 	}
 
-	warning(payload: LogPayload<unknown>): void {
-		this._winston.warn(util.format(payload.message))
+	public warn(payload: LogPayload<unknown>): void {
+		this._winston.warn(util.format(payload.message));
 	}
 
-	info(payload: LogPayload<unknown>): void {
-		this._winston.info(payload.message)
+	public info(payload: LogPayload<unknown>): void {
+		this._winston.info(payload.message);
 	}
 
-	debug(payload: LogPayload<unknown>): void {
-		this._winston.debug(payload.message)
+	public debug(payload: LogPayload<unknown>): void {
+		this._winston.debug(payload.message);
 	}
-
 }
