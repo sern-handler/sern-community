da771a51701d585f79beb6ea00a1d1e84f4d6221
EvolutionX <evolutionx9777@gmail.com>
2023-01-04 18:23:32 +0530

    chore: #grammarnazi
    permalink: http://whatthecommit.com/2c3e72a2b0f89d485865f7d43303e5a2

---
+++ src/utils/Logger.ts
@@ -0,0 +1,40 @@
+import type { Logging, LogPayload } from "@sern/handler";
+import winston from "winston";
+import util from "util";
+export class SernLogger implements Logging {
+	private _winston!: winston.Logger;
+
+	public constructor(level: string, isProd = false) {
+		this._winston = winston.createLogger({
+			level,
+			format: winston.format.json(),
+		});
+		if (!isProd) {
+			this._winston.add(
+				new winston.transports.Console({
+					format: winston.format.simple(),
+				})
+			);
+		} else {
+			this._winston.add(
+				new winston.transports.File({ filename: "combined.log" })
+			);
+		}
+	}
+
+	public error(payload: LogPayload<unknown>): void {
+		this._winston.error(payload.message);
+	}
+
+	public warn(payload: LogPayload<unknown>): void {
+		this._winston.warn(util.format(payload.message));
+	}
+
+	public info(payload: LogPayload<unknown>): void {
+		this._winston.info(payload.message);
+	}
+
+	public debug(payload: LogPayload<unknown>): void {
+		this._winston.debug(payload.message);
+	}
+}
