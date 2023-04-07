75f9c0bae229e8023556d1272e2649c7a8e5b1b6
EvolutionX <evolutionx9777@gmail.com>
2023-04-07 09:55:05 +0530

    chore: and if you ask me how i'm feeling
    permalink: http://whatthecommit.com/d5526638f004314a5848b61b41a0a965

--- src/events/triggers.ts
+++ src/events/triggers.ts
@@ -6,7 +6,7 @@ export default eventModule({
 	type: EventType.Discord,
 	async execute(message: Message) {
 		if (message.author?.bot || message.webhookId) return;
-		const regex = /w+h+a+t+(?:'s+| is+|s+|’s+)? (?:.*)?b+o+f+a+/gim;
+		const regex = /(?:w+h+a+t+)?(?:'s+| is+|s+|’s+)? ?(?:.*)?b+o+f+a+/gmi;
 
 		if (message.content.match(regex)) {
 			return message.reply("bofa deez nuts");
