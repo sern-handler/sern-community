2b03cc2b693080199dcd817e38124eeb29a88266
EvolutionX <evolutionx9777@gmail.com>
2023-04-07 09:48:33 +0530

    chore: no time to commit.. my people need me!
    permalink: http://whatthecommit.com/fbe8992c2d5d5a097c6ffbf42932fbf3

--- src/events/triggers.ts
+++ src/events/triggers.ts
@@ -6,7 +6,7 @@ export default eventModule({
 	type: EventType.Discord,
 	async execute(message: Message) {
 		if (message.author?.bot || message.webhookId) return;
-		const regex = /w+h+a+t+(?:'s+| is+|s)? (?:.*)?b+o+f+a+/gim;
+		const regex = /w+h+a+t+(?:'s+| is+|s+|’s+)? (?:.*)?b+o+f+a+/gim;
 
 		if (message.content.match(regex)) {
 			return message.reply("bofa deez nuts");
