d4628988dbf322338425d3c348643a81969172e8
kingomes <83099848+kingomes@users.noreply.github.com>
2024-08-10 00:31:30 -0500

    15+ minute should work

--- src/commands/giveaway.ts
+++ src/commands/giveaway.ts
@@ -1,4 +1,4 @@
-import { commandModule, CommandType } from "@sern/handler";
+import { commandModule, CommandType, scheduledTask } from "@sern/handler";
 import { publish } from "#plugins";
 import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 import { db } from "../utils/db.js";
@@ -28,7 +28,8 @@ export default commandModule({
         let timeLeft = Number(timeLeftString?.substring(0, timeLeftString.length - 1))
 
         const startTime = new Date()
-        let endTime
+
+        let endTime: Date
         if (timeUnit === 's') {
             endTime = new Date(startTime.getTime() + timeLeft! * 1000)
         }
@@ -38,12 +39,13 @@ export default commandModule({
         else if (timeUnit === 'h') {
             endTime = new Date(startTime.getTime() + timeLeft! * 3600000)
         }
-        const endTimeStamp = `<t:${Math.floor(endTime!.getTime() / 1000)}:f>`
+        const endTimeStamp: string = `<t:${Math.floor(endTime!.getTime() / 1000)}:f>`
 
         let embed = new EmbedBuilder()
         .setTitle(`${item} Giveaway`)
         .addFields(
             {name: '\u200b', value: 'React to enter the giveaway!'},
+            {name: '\u200b', value: `Time: ${timeLeft}${timeUnit}`},
             {name: '\u200b', value: `Ends at: ${endTimeStamp}`}
         )
         
@@ -52,68 +54,35 @@ export default commandModule({
             embeds: [embed],
         }).then(embedMessage => {
             embedMessage.react("🎉")
+
+            let interval
+        
+            if (timeUnit === 's') {
+                interval = 1000
+            }
+            else if (timeUnit === 'm') {
+                interval = 60000
+            }
+            else if (timeUnit === 'h') {
+                interval = 3600000
+            }
+
+            setInterval(() => {
+                const stmt = db.prepare(`SELECT * FROM entrees`).all()
+        
+                const winnerIndex = Math.floor(Math.random() * stmt.length)
+                const winnerId = stmt[winnerIndex].user_id
+                const winner = ctx.guild?.members.cache.get(winnerId)
+                const winnerName = winner?.user.globalName
+
+                embed.setFields(
+                    {name: '\u200b', value: `Winner: @${winnerName}`},
+                    {name: '\u200b', value: `Ended at: ${endTimeStamp}`}
+                )
+
+                embedMessage.edit({embeds: [embed]})
+            }, timeLeft * interval)
         })
-        
-        if (timeUnit === 's') {
-            setInterval(() => {
-                if (timeLeft !== 0) {
-                    timeLeft = 0
-
-                    const stmt = db.prepare(`SELECT * FROM entrees`).all()
-        
-                    const winnerIndex = Math.floor(Math.random() * stmt.length)
-                    const winnerId = stmt[winnerIndex].user_id
-                    const winner = ctx.guild?.members.cache.get(winnerId)
-                    const winnerName = winner?.user.globalName
-
-                    embed.setFields(
-                        {name: '\u200b', value: `Winner: @${winnerName}`},
-                        {name: '\u200b', value: `Ended at: ${endTimeStamp}`}
-                    )
-                    ctx.interaction.editReply({ embeds: [embed] })
-                }
-            }, timeLeft * 1000) 
-        }
-        else if (timeUnit === 'm') {
-            setInterval(() => {
-                if (timeLeft !== 0) {
-                    timeLeft = 0
-
-                    const stmt = db.prepare(`SELECT * FROM entrees`).all()
-        
-                    const winnerIndex = Math.floor(Math.random() * stmt.length)
-                    const winnerId = stmt[winnerIndex].user_id
-                    const winner = ctx.guild?.members.cache.get(winnerId)
-                    const winnerName = winner?.user.globalName
-
-                    embed.setFields(
-                        {name: '\u200b', value: `Winner: @${winnerName}`},
-                        {name: '\u200b', value: `Ended at: ${endTimeStamp}`}
-                    )
-                    ctx.interaction.editReply({ embeds: [embed] })
-                }
-            }, timeLeft * 60000) 
-        }
-        if (timeUnit === 'h') {
-            setInterval(() => {
-                if (timeLeft !== 0) {
-                    timeLeft = 0
-
-                    const stmt = db.prepare(`SELECT * FROM entrees`).all()
-        
-                    const winnerIndex = Math.floor(Math.random() * stmt.length)
-                    const winnerId = stmt[winnerIndex].user_id
-                    const winner = ctx.guild?.members.cache.get(winnerId)
-                    const winnerName = winner?.user.globalName
-                    
-                    embed.setFields(
-                        {name: '\u200b', value: `Winner: @${winnerName}`},
-                        {name: '\u200b', value: `Ended at: ${endTimeStamp}`}
-                    )
-                    ctx.interaction.editReply({ embeds: [embed] })
-                }
-            }, timeLeft * 3600000) 
-        }
 
         db.prepare(`DELETE FROM entrees`).run()
     }
