a7ccd0a369a0f110a992922a9c4a08a281e1d21c
kingomes <83099848+kingomes@users.noreply.github.com>
2024-08-23 23:35:11 -0500

    date-fns + host excluded

--- src/commands/giveaway.ts
+++ src/commands/giveaway.ts
@@ -2,6 +2,8 @@ import { commandModule, CommandType, scheduledTask } from "@sern/handler";
 import { publish } from "#plugins";
 import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 import { db } from "../utils/db.js";
+import { add, addDays, addHours, addMinutes, addSeconds } from "date-fns"
+import { Timestamp } from "#utils";
 
 export default commandModule({
     type: CommandType.Slash,
@@ -16,65 +18,93 @@ export default commandModule({
         },
         {
             name: "time",
-            description: "The amount of time that the giveaway will be up (end with s, m, or h)",
+            description: "The amount of time that the giveaway will be up",
             type: ApplicationCommandOptionType.String,
             required: true
         }
     ],
-    execute: async (ctx) => {
-        const item = ctx.options.getString("item")
-        let timeLeftString = ctx.options.getString("time")
-        let timeUnit = timeLeftString?.substring(timeLeftString.length - 1)
-        let timeLeft = Number(timeLeftString?.substring(0, timeLeftString.length - 1))
+    execute: async (ctx, { deps }) => {
+            const item = ctx.options.getString("item")
+            let timeLeftString = ctx.options.getString("time")
 
-        const startTime = new Date()
+            let timeUnit1
+            let timeLeft1
+            let timeUnit2
+            let timeLeft2
+            if (!timeLeftString?.includes("and")) {
+                timeUnit1 = timeLeftString?.split(" ")[1]
+                timeLeft1 = Number(timeLeftString?.split(" ")[0])
+            }
+            else {
+                let timeLeftStringPart1 = timeLeftString.split("and")[0]
+                timeUnit1 = timeLeftStringPart1?.split(" ")[1]
+                timeLeft1 = Number(timeLeftStringPart1?.split(" ")[0])
 
-        let endTime: Date
-        if (timeUnit === 's') {
-            endTime = new Date(startTime.getTime() + timeLeft! * 1000)
-        }
-        else if (timeUnit === 'm') {
-            endTime = new Date(startTime.getTime() + timeLeft! * 60000)
-        }
-        else if (timeUnit === 'h') {
-            endTime = new Date(startTime.getTime() + timeLeft! * 3600000)
-        }
-        const endTimeStamp: string = `<t:${Math.floor(endTime!.getTime() / 1000)}:f>`
+                let timeLeftStringPart2 = timeLeftString.split("and")[1].replace(timeLeftString.split("and")[1].substring(0, 1), "")
+                timeUnit2 = timeLeftStringPart2?.split(" ")[1]
+                timeLeft2 = Number(timeLeftStringPart2?.split(" ")[0])
+            }
 
-        let embed = new EmbedBuilder()
-        .setTitle(`🥳 ${item} giveaway 🥳`)
-        .setDescription('React to enter the giveaway!')
-        .addFields(
-            {name: '\u200b', value: `Hosted by: <@${ctx.userId}>`},
-            {name: '\u200b', value: `Ends at: ${endTimeStamp}`}
-        )
-        
+            const startTime = new Date()
 
-        await ctx.reply({
-            embeds: [embed],
-        }).then(embedMessage => {
-            embedMessage.react("🎉")
+            let endTime: Date
 
-            let interval = setInterval(() => {
-                let now = new Date().getTime()
+            let secondNames = ['seconds', 'second', 'sec', 'secs']
+            let minuteNames = ['minutes', 'minute', 'min', 'mins']
+            let hourNames = ['hours', 'hour', 'hr', 'hrs']
+            let dayNames = ['days', 'day']
 
-                let distance = endTime.getTime() - now
+            endTime = add(startTime, {
+                timeUnit1: timeLeft1,
+                timeUnit2: timeLeft2
+            })
 
-                let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-                let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-                let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+            if (secondNames.includes(timeUnit1!)) {
+                endTime = endTime === startTime ? addSeconds(startTime, timeLeft1) : addSeconds(endTime, timeLeft1)
+            }
+            if (secondNames.includes(timeUnit2!)) {
+                endTime = endTime === startTime ? addSeconds(startTime, timeLeft2!) : addSeconds(endTime, timeLeft2!)
+            }
+            if (minuteNames.includes(timeUnit1!)) {
+                endTime = endTime === startTime ? addMinutes(startTime, timeLeft1) : addMinutes(endTime, timeLeft1)
+            }
+            if (minuteNames.includes(timeUnit2!)) {
+                endTime = endTime === startTime ? addMinutes(startTime, timeLeft2!) : addMinutes(endTime, timeLeft2!)
+            }
+            if (hourNames.includes(timeUnit1!)) {
+                endTime = endTime === startTime ? addHours(startTime, timeLeft1) : addHours(endTime, timeLeft1)
+            }
+            if (hourNames.includes(timeUnit2!)) {
+                endTime = endTime === startTime ? addHours(startTime, timeLeft2!) : addHours(endTime, timeLeft2!)
+            }
+            if (dayNames.includes(timeUnit1!)) {
+                endTime = endTime === startTime ? addDays(startTime, timeLeft1) : addDays(endTime, timeLeft1)
+            }
+            if (dayNames.includes(timeUnit2!)) {
+                endTime = endTime === startTime ? addDays(startTime, timeLeft2!) : addDays(endTime, timeLeft2!)
+            }
 
-                if (distance >= 0) {
-                    embed.setFields(
-                        {name: '\u200b', value: `Time Left: ${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`},
-                        {name: '\u200b', value: `Hosted by: <@${ctx.userId}>`},
-                        {name: '\u200b', value: `Ends at: ${endTimeStamp}`}
-                    )
+            const endTimeStamp: string = `<t:${Math.floor(endTime!.getTime() / 1000)}:f>`
+            const endTimeStamp2 = new Timestamp(endTime.getTime()).timestamp
 
-                    embedMessage.edit({embeds: [embed]})
-                }
+            let embed = new EmbedBuilder()
+            .setTitle(`🥳 ${item} giveaway 🥳`)
+            .setDescription('React to enter the giveaway!')
+            .addFields(
+                {name: '\u200b', value: `Hosted by: <@${ctx.userId}>`},
+                {name: '\u200b', value: `Time Left: ${new Timestamp(Number(endTimeStamp2)).getRelativeTime()}`},
+                {name: '\u200b', value: `Ends at: ${endTimeStamp}`}
+            )
+            
 
-                else if (distance < 0) {
+            await ctx.reply({
+                embeds: [embed],
+            }).then(embedMessage => {
+                embedMessage.react("🎉")
+
+                let intervalTime = endTime.getTime() - startTime.getTime()
+
+                let interval = setInterval(() => {
                     const stmt = db.prepare(`SELECT * FROM entrees`).all()
 
                     let winnerIndex = Math.floor(Math.random() * stmt.length)
@@ -95,7 +125,6 @@ export default commandModule({
                     }
                     else if (stmt.length > 1 && stmt[winnerIndex].user_id === ctx.userId) {
                         winnerIndex = Math.floor(Math.random() * stmt.length)
-                        console.log(winnerIndex)
                     }
                     else if ((stmt.length === 1 && stmt[winnerIndex].user_id === ctx.userId) || stmt.length === 0) {
                         embed.setDescription('\u200b')
@@ -109,8 +138,7 @@ export default commandModule({
 
                         clearInterval(interval)
                     }
-                }
-            }, 1000)
+            }, intervalTime)
         })
 
         db.prepare(`DELETE FROM entrees`).run()
