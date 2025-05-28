174914bfffeb73406dc5f1c76c530078853784ac
kingomes <83099848+kingomes@users.noreply.github.com>
2025-05-28 00:36:46 -0500

    using prettier to format

--- src/commands/giveaway.ts
+++ src/commands/giveaway.ts
@@ -1,8 +1,14 @@
 import { commandModule, CommandType } from "@sern/handler";
 import { ownerOnly, publish } from "#plugins";
-import { ApplicationCommandOptionType, ButtonBuilder, ActionRowBuilder, ButtonStyle, EmbedBuilder } from "discord.js";
+import {
+    ApplicationCommandOptionType,
+    ButtonBuilder,
+    ActionRowBuilder,
+    ButtonStyle,
+    EmbedBuilder,
+} from "discord.js";
 import { db } from "../utils/db.js";
-import { add } from "date-fns"
+import { add } from "date-fns";
 import { Timestamp } from "#utils";
 
 export default commandModule({
@@ -14,68 +20,93 @@ export default commandModule({
             name: "item",
             description: "The item that will be given away",
             type: ApplicationCommandOptionType.String,
-            required: true
+            required: true,
         },
         {
             name: "time",
             description: "The amount of time that the giveaway will be up",
             type: ApplicationCommandOptionType.String,
-            required: true
-        }
+            required: true,
+        },
     ],
     execute: async (ctx, { deps }) => {
-            const item = ctx.options.getString("item")
-            const timeLeftString = ctx.options.getString("time", true)
+        const item = ctx.options.getString("item");
+        const timeLeftString = ctx.options.getString("time", true);
 
-            let timeUnit1
-            let timeLeft1
-            let timeUnit2
-            let timeLeft2
+        let timeUnit1;
+        let timeLeft1;
+        let timeUnit2;
+        let timeLeft2;
 
-            const [part1, part2] = timeLeftString?.split("and")
-            timeUnit1 = part1?.split(" ")[1]
-            timeLeft1 = Number(part1?.split(" ")[0])
+        const [part1, part2] = timeLeftString?.split("and");
+        timeUnit1 = part1?.split(" ")[1];
+        timeLeft1 = Number(part1?.split(" ")[0]);
 
-            if (part2) {
-                const timeLeftStringPart2 = part2.replace(part2.substring(0, 1), "")
-                timeUnit2 = timeLeftStringPart2?.split(" ")[1]
-                timeLeft2 = Number(timeLeftStringPart2?.split(" ")[0])
-            }
+        if (part2) {
+            const timeLeftStringPart2 = part2.replace(part2.substring(0, 1), "");
+            timeUnit2 = timeLeftStringPart2?.split(" ")[1];
+            timeLeft2 = Number(timeLeftStringPart2?.split(" ")[0]);
+        }
 
-            const startTime = new Date()
+        const startTime = new Date();
 
-            let endTime: Date
+        let endTime: Date;
 
-            const secondNames = ['seconds', 'second', 'sec', 'secs']
-            const minuteNames = ['minutes', 'minute', 'min', 'mins']
-            const hourNames = ['hours', 'hour', 'hr', 'hrs']
-            const dayNames = ['days', 'day']
+        const secondNames = ["seconds", "second", "sec", "secs"];
+        const minuteNames = ["minutes", "minute", "min", "mins"];
+        const hourNames = ["hours", "hour", "hr", "hrs"];
+        const dayNames = ["days", "day"];
 
-            endTime = add(startTime, {
-                seconds: secondNames.includes(timeUnit1!) ? timeLeft1 : secondNames.includes(timeUnit2!) ? timeLeft2 : 0,
-                minutes: minuteNames.includes(timeUnit1!) ? timeLeft1 : minuteNames.includes(timeUnit2!) ? timeLeft2 : 0,
-                hours: hourNames.includes(timeUnit1!) ? timeLeft1 : hourNames.includes(timeUnit2!) ? timeLeft2 : 0,
-                days: dayNames.includes(timeUnit1!) ? timeLeft1 : dayNames.includes(timeUnit2!) ? timeLeft2 : 0
-            })
+        endTime = add(startTime, {
+            seconds: secondNames.includes(timeUnit1!)
+                ? timeLeft1
+                : secondNames.includes(timeUnit2!)
+                ? timeLeft2
+                : 0,
+            minutes: minuteNames.includes(timeUnit1!)
+                ? timeLeft1
+                : minuteNames.includes(timeUnit2!)
+                ? timeLeft2
+                : 0,
+            hours: hourNames.includes(timeUnit1!)
+                ? timeLeft1
+                : hourNames.includes(timeUnit2!)
+                ? timeLeft2
+                : 0,
+            days: dayNames.includes(timeUnit1!)
+                ? timeLeft1
+                : dayNames.includes(timeUnit2!)
+                ? timeLeft2
+                : 0,
+        });
 
-            const endTimeStamp: string = `<t:${Math.floor(endTime!.getTime() / 1000)}:f>`
-            const endTimeStamp2 = new Timestamp(endTime.getTime()).timestamp
+        const endTimeStamp: string = `<t:${Math.floor(endTime!.getTime() / 1000)}:f>`;
+        const endTimeStamp2 = new Timestamp(endTime.getTime()).timestamp;
 
-            let embed = new EmbedBuilder()
+        let embed = new EmbedBuilder()
             .setTitle(`🥳 ${item} giveaway 🥳`)
-            .setDescription('React to enter the giveaway!')
+            .setDescription("React to enter the giveaway!")
             .addFields(
-                {name: '\u200b', value: `Hosted by: <@${ctx.userId}>`},
-                {name: '\u200b', value: `Ends: ${new Timestamp(Number(endTimeStamp2)).getRelativeTime()} (${endTimeStamp})`}
-            )
-            
+                { name: "\u200b", value: `Hosted by: <@${ctx.userId}>` },
+                {
+                    name: "\u200b",
+                    value: `Ends: ${new Timestamp(
+                        Number(endTimeStamp2)
+                    ).getRelativeTime()} (${endTimeStamp})`,
+                }
+            );
 
-            await ctx.reply({
+        await ctx
+            .reply({
                 embeds: [embed],
-            }).then(embedMessage => {
-                let giveawayEnded = false
+            })
+            .then((embedMessage) => {
+                let giveawayEnded = false;
 
-                db.prepare(`INSERT INTO giveaway_message(message_id, host_id) VALUES (?, ?)`).run(embedMessage.id, ctx.userId)
+                db.prepare(`INSERT INTO giveaway_message(message_id, host_id) VALUES (?, ?)`).run(
+                    embedMessage.id,
+                    ctx.userId
+                );
 
                 // test entries
                 // db.prepare(`INSERT INTO entries(message_id, timestamp, user_id) VALUES (?, ?, ?)`).run([embedMessage.id, 1, 1])
@@ -84,71 +115,96 @@ export default commandModule({
                 // db.prepare(`INSERT INTO entries(message_id, timestamp, user_id) VALUES (?, ?, ?)`).run([embedMessage.id, 4, 4])
                 // db.prepare(`INSERT INTO entries(message_id, timestamp, user_id) VALUES (?, ?, ?)`).run([embedMessage.id, 5, 5])
 
-                embedMessage.react("🎉")
+                embedMessage.react("🎉");
 
                 //checks if author reacted to itself
                 const selfReactionInterval = setInterval(() => {
-                    const userReactions = embedMessage.reactions.cache.filter(reaction => reaction.users.cache.has(ctx.userId))
-            
+                    const userReactions = embedMessage.reactions.cache.filter((reaction) =>
+                        reaction.users.cache.has(ctx.userId)
+                    );
+
                     for (const reaction of userReactions.values()) {
                         reaction.users.remove(ctx.userId);
-                        ctx.interaction.followUp({content: "As the host of the giveaway, you cannot enter it.", ephemeral: true})
+                        ctx.interaction.followUp({
+                            content: "As the host of the giveaway, you cannot enter it.",
+                            ephemeral: true,
+                        });
                     }
-                }, 1000)
+                }, 1000);
 
-                let intervalTime = endTime.getTime() - startTime.getTime()
+                let intervalTime = endTime.getTime() - startTime.getTime();
 
                 setTimeout(() => {
-                    const stmt = db.prepare(`SELECT * FROM entries WHERE message_id = ?`).all(embedMessage.id)
+                    const stmt = db
+                        .prepare(`SELECT * FROM entries WHERE message_id = ?`)
+                        .all(embedMessage.id);
 
-                    const eligible = stmt.filter((entry: { user_id: string; }) => entry.user_id !== embedMessage.author.id && entry.user_id !== ctx.user.id)
+                    const eligible = stmt.filter(
+                        (entry: { user_id: string }) =>
+                            entry.user_id !== embedMessage.author.id &&
+                            entry.user_id !== ctx.user.id
+                    );
 
-                    let winnerIndex = Math.floor(Math.random() * eligible.length)
+                    let winnerIndex = Math.floor(Math.random() * eligible.length);
 
                     if (eligible.length > 0 && eligible[winnerIndex].user_id !== ctx.userId) {
-                        const winnerId = stmt[winnerIndex].user_id
+                        const winnerId = stmt[winnerIndex].user_id;
 
-                        embedMessage.edit({content: `Congratulations <@${winnerId}> on winning the ${item} giveaway! ${eligible.length} users entered`, embeds: []})
-                        giveawayEnded = true
-                    }
-                    else if (eligible.length > 1 && eligible[winnerIndex].user_id === ctx.userId) {
+                        embedMessage.edit({
+                            content: `Congratulations <@${winnerId}> on winning the ${item} giveaway! ${eligible.length} users entered`,
+                            embeds: [],
+                        });
+                        giveawayEnded = true;
+                    } else if (
+                        eligible.length > 1 &&
+                        eligible[winnerIndex].user_id === ctx.userId
+                    ) {
                         while (eligible[winnerIndex].user_id === ctx.userId) {
-                            winnerIndex = Math.floor(Math.random() * eligible.length)
+                            winnerIndex = Math.floor(Math.random() * eligible.length);
                         }
-                        const winnerId = eligible[winnerIndex].user_id
+                        const winnerId = eligible[winnerIndex].user_id;
 
-                        embedMessage.edit({content: `Congratulations <@${winnerId}> on winning the ${item} giveaway! ${eligible.length} users entered`, embeds: []})
-                        giveawayEnded = true
-
-                    }
-                    else if ((eligible.length === 1 && eligible[winnerIndex].user_id === ctx.userId) || eligible.length === 0) {
-                        embedMessage.edit({content: `Couldn't determine a winner: Not enough eligible users. ${eligible.length} users entered`, embeds: [], components: [retryRows()]})
+                        embedMessage.edit({
+                            content: `Congratulations <@${winnerId}> on winning the ${item} giveaway! ${eligible.length} users entered`,
+                            embeds: [],
+                        });
+                        giveawayEnded = true;
+                    } else if (
+                        (eligible.length === 1 && eligible[winnerIndex].user_id === ctx.userId) ||
+                        eligible.length === 0
+                    ) {
+                        embedMessage.edit({
+                            content: `Couldn't determine a winner: Not enough eligible users. ${eligible.length} users entered`,
+                            embeds: [],
+                            components: [retryRows()],
+                        });
                     }
 
                     if (giveawayEnded) {
-                        db.prepare(`DELETE FROM giveaway_message WHERE message_id = ?`).run(embedMessage.id)
-                        db.prepare(`DELETE FROM entries WHERE message_id = ?`).run(embedMessage.id)
-                        embedMessage.reactions.removeAll()
+                        db.prepare(`DELETE FROM giveaway_message WHERE message_id = ?`).run(
+                            embedMessage.id
+                        );
+                        db.prepare(`DELETE FROM entries WHERE message_id = ?`).run(embedMessage.id);
+                        embedMessage.reactions.removeAll();
                     }
-                    clearInterval(selfReactionInterval)
-            }, intervalTime)
-        })
-    }
-})
+                    clearInterval(selfReactionInterval);
+                }, intervalTime);
+            });
+    },
+});
 
 function retryRows() {
     const attemptReroll = new ButtonBuilder({
-        customId: 'reroll',
-        label: 'Attempt Reroll',
-        style: ButtonStyle.Primary
-    })
+        customId: "reroll",
+        label: "Attempt Reroll",
+        style: ButtonStyle.Primary,
+    });
 
     const discardGiveaway = new ButtonBuilder({
-        customId: 'discard',
-        label: 'Discard Giveaway',
-        style: ButtonStyle.Primary
-    })
+        customId: "discard",
+        label: "Discard Giveaway",
+        style: ButtonStyle.Primary,
+    });
 
-    return new ActionRowBuilder<ButtonBuilder>()
-        .addComponents(attemptReroll, discardGiveaway);
+    return new ActionRowBuilder<ButtonBuilder>().addComponents(attemptReroll, discardGiveaway);
 }
--- src/commands/handlers/giveawayDiscard.ts
+++ src/commands/handlers/giveawayDiscard.ts
@@ -10,13 +10,13 @@ export default commandModule({
             return ctx.reply({
                 ephemeral: true,
                 content: `You cannot discard the giveaway because you are not one of the owners`,
-            })
+            });
 
-        db.prepare(`DELETE FROM giveaway_message WHERE message_id = ?`).run(ctx.message.id)
-        db.prepare(`DELETE FROM entries WHERE message_id = ?`).run(ctx.message.id)
+        db.prepare(`DELETE FROM giveaway_message WHERE message_id = ?`).run(ctx.message.id);
+        db.prepare(`DELETE FROM entries WHERE message_id = ?`).run(ctx.message.id);
 
-        ctx.message.reactions.removeAll()
-        ctx.message.delete()
-        await ctx.reply({ephemeral: true, content: `Giveaway discarded!`})
+        ctx.message.reactions.removeAll();
+        ctx.message.delete();
+        await ctx.reply({ ephemeral: true, content: `Giveaway discarded!` });
     },
 });
